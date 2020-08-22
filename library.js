1841a80f298f22a707a6a67b031b4fb1050f6b7c
oplik0 <25460763+oplik0@users.noreply.github.com>
2020-08-22 17:06:42 +0200

    fix: working checkboxes

--- library.js
+++ library.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-const user = require.main.require('./src/user');
+const user = require.main.require("./src/user");
 
 const discordEmbed = {};
 let app;
@@ -11,23 +11,31 @@ discordEmbed.init = async function (params) {
 
 discordEmbed.getWidgets = async function (widgets) {
 	const discordWidget = {
-		name: 'Discord Chat',
-		widget: 'discord-chat',
-		description: 'Discord chat embed powered by TitanEmbeds',
-		content: await app.renderAsync('admin/discordWidget', {}),
+		name: "Discord Chat",
+		widget: "discord-chat",
+		description: "Discord chat embed powered by TitanEmbeds",
+		content: await app.renderAsync("admin/discordWidget", {}),
 	};
 	widgets.push(discordWidget);
 	return widgets;
 };
 discordEmbed.renderDiscordWidget = async function (widget) {
 	const data = widget.data;
-	if (data.serverId.slice(0, 4) === 'http') {
+	data.css = data.css !== "" ? parseInt(data.css, 10) : 0;
+	data.fixedsidenav =
+		data.fixedsidenav && data.fixedsidenav !== "" ? "true" : "false";
+	data.noscroll = data.noscroll && data.noscroll !== "" ? "true" : "false";
+	data.lockscrollbar =
+		data.lockscrollbar && data.lockscrollbar !== "" ? "true" : "false";
+	data.userscalable =
+		data.userscalable && data.userscalable !== "" ? "true" : "false";
+	if (data.serverId.slice(0, 4) === "http") {
 		data.serverId = data.serverId
-			.slice(data.serverId.slice(0, -1).lastIndexOf('/'))
-			.replaceAll('/', '');
+			.slice(data.serverId.slice(0, -1).lastIndexOf("/"))
+			.replaceAll("/", "");
 	}
-	data.username = await user.getUsernamesByUids([widget.uid])[0];
-	widget.html = await app.renderAsync('widgets/discordChat', data);
+	[data.username] = await user.getUsernamesByUids([widget.uid]);
+	widget.html = await app.renderAsync("widgets/discordChat", data);
 	return widget;
 };
 
--- static/templates/widgets/discordChat.tpl
+++ static/templates/widgets/discordChat.tpl
@@ -1,5 +1,5 @@
 <iframe
-    class="discordChat"
-    src="https://titanembeds.com/embed/{serverId}?css={css}&defaultchannel={defaultchannel}&fixedsidenav={fixedsidenav}&lang={lang}&noscroll={noscroll}&scrollbartheme={scrollbartheme}&lockscrollbar={lockscrollbar}&theme={theme}&username={username}&userscalable={userscalable}"
-    frameborder="0"
+  class="discordChat"
+  src="https://titanembeds.com/embed/{serverId}?username={username}&css={css}&defaultchannel={defaultchannel}&fixedsidenav={fixedsidenav}&lang={lang}&noscroll={noscroll}&scrollbartheme={scrollbartheme}&lockscrollbar={lockscrollbar}&theme={theme}&userscalable={userscalable}"
+  frameborder="0"
 ></iframe>
